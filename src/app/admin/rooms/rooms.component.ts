f9001646311b2fc28a75027ef7360e09026078a9
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-11-08 16:59:07 +0800

    implements rooms component

--- src/app/admin/rooms/rooms.component.ts
+++ src/app/admin/rooms/rooms.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Room } from 'src/app/model/Room';
 
 @Component({
-  selector: 'app-rooms',
+  selector: 'rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  rooms!: Array<Room>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.subscribeRooms();
   }
 
+
+  private subscribeRooms() {
+    this.dataService.getRooms().subscribe(
+      rooms => {
+        this.rooms = rooms;
+      },
+      errors => {
+        console.log('get rooms fail');
+      }
+    );
+  }
 }
