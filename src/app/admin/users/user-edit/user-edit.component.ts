efdce98b0429c0bce9f306423c2a8e2ad6e73bbc
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-11-09 02:27:17 +0800

    implements user form validation

--- src/app/admin/users/user-edit/user-edit.component.ts
+++ src/app/admin/users/user-edit/user-edit.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/data.service';
 import { User } from 'src/app/model/User';
-
 @Component({
   selector: 'user-edit',
   templateUrl: './user-edit.component.html',
@@ -14,6 +13,12 @@ export class UserEditComponent implements OnInit {
   user!: User;
   message!: string;
   formUser!: User;
+  password!: string;
+  password2!: string;
+
+  nameIsValid = false;
+  passwordIsValid = false;
+  passwordIsMatch = false;
 
   constructor(
     private dataService: DataService,
@@ -22,6 +27,8 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.duplicateUserObjectToForm();
+    this.checkNameIsValid();
+    this.checkPasswordIsValid();
   }
 
   private duplicateUserObjectToForm() {
@@ -58,4 +65,21 @@ export class UserEditComponent implements OnInit {
     else              // under add mode
       this.saveAddUser();
   }
+
+  checkPasswordIsValid(){
+    if(this.formUser.id){   // under edit mode
+      this.passwordIsValid = true;
+      this.passwordIsMatch = true;
+    }else{              // under add mode
+      this.passwordIsMatch = this.password === this.password2;
+      if(this.password)   // password could be null
+        this.passwordIsValid = this.password.trim().length > 0;
+    }
+  }
+
+  checkNameIsValid(){
+    if(this.formUser.name)  // formUser.name could be null
+      this.nameIsValid = this.formUser.name.trim().length > 0;
+  }
+
 }
